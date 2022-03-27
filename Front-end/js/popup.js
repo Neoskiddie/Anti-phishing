d4f1b44642896353f3246253564e7e58fde9a9cf
Windows <voldig@gmail.com>
2022-03-27 19:50:03 +0100

    working js for popup slider

--- Front-end/js/popup.js
+++ Front-end/js/popup.js
@@ -1,6 +1,6 @@
 document.getElementById('options').onclick = openOps;
 document.getElementById('about').onclick = openAbout;
-const checkbox = document.getElementById("checkbox");
+var checkbox = document.getElementById("checkbox");
 function openOps() {
     chrome.runtime.openOptionsPage()
 };
@@ -8,3 +8,29 @@ function openOps() {
 function openAbout() {
     chrome.tabs.create({ url: 'https://gbronka.com/honours' });
 };
+
+
+// async method to get storage value
+const readLocalStorage = async (key) => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get([key], function (result) {
+      if (result[key] === undefined) {
+        reject();
+      } else {
+        resolve(result[key]);
+      }
+    });
+  });
+};
+
+// load enabled from storage and set the checkbox to it's value
+window.onload = async () => {
+  let enabled = await readLocalStorage('enabled');
+  checkbox.checked = enabled;
+};
+
+// set the checkbox value
+checkbox.addEventListener("change", (event) => {
+  const enabled = event.target.checked;
+  chrome.storage.local.set({ enabled });
+});
